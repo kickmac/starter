d052c0c4ca41061bd64d087978c9b0deb9b8b6af
kikuchi <kikuchi@shift-jp.net>
2018-03-09 17:52:02 +0900

    gulp task pageList修正

--- gulp/tasks/suite.js
+++ gulp/tasks/suite.js
@@ -3,6 +3,6 @@ module.exports = function(gulp, $, config) {
 	var ejsConfig = require('../ejs_config.js');
 
 	gulp.task(taskName, function () {
-		return $.runSequence(['html', 'svgSprite', 'iconFont', 'pageList', 'js'] , 'css')
+		return $.runSequence(['html', 'svgSprite', 'iconFont', 'js'] , 'css')
 	});
 };
