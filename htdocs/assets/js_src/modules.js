747f8ed29b1c8fd6484648657fcd70f4cf41d23a
kikuchi <kikuchi@shift-jp.net>
2018-08-22 15:54:28 +0900

    pageListをvueに

--- htdocs/assets/js_src/modules.js
+++ htdocs/assets/js_src/modules.js
@@ -8,6 +8,20 @@ $.fn.extend({
 var $$$ = $$$ || {};
 
 /*************************************************************************************
+* vh adjust
+*************************************************************************************/
+$$$.vhAdjust = (function(){
+	var _vh = 0;
+	var _init = function(){
+		_vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', _vh + 'px');
+	}
+	return {
+		init: _init,
+	}
+}());
+
+/*************************************************************************************
 * windowInfo
 *************************************************************************************/
 $$$.windowInfo = (function(){
@@ -668,6 +682,56 @@ $$$.contentsModal = (function() {
 		close: _close,
 	};
 }());
+
+/*************************************************************************************
+* dummyImage生成
+*************************************************************************************/
+$$$.dummyImage = (function(){
+	var _init = function(){
+		$(this).each(function(index, el) {
+			_setImg.call($(this));
+		});
+	}
+	var _imageToBase64 = function(option){
+		var canvas = document.createElement('canvas');
+		canvas.width  = option.w;
+		canvas.height = option.h;
+		var ctx = canvas.getContext('2d');
+
+		//背景
+		ctx.fillStyle = option.background;
+		ctx.fillRect(0, 0, option.w, option.h);
+		ctx.lineWidth = 1;
+		ctx.strokeStyle = option.borderColor;
+		ctx.strokeRect(0, 0, option.w, option.h)
+
+		//文字
+		ctx.textBaseline = 'middle';
+		ctx.fillStyle = option.color;
+		ctx.font = option.fontSize + ' ' + option.fontFamily;
+		ctx.textAlign = "center";
+		ctx.fillText(option.str, canvas.width/2, canvas.height/2);
+		return canvas.toDataURL('image/png');
+	}
+	var _setImg = function(){
+		var _options = $(this).data('options')
+		$(this).attr('src', _imageToBase64({
+			str: _options.str || '',
+			w: _options.w || 300,
+			h: _options.h || 300,
+			fontSize: _options.fontSize || '16px',
+			fontFamily: _options.fontFamily || 'Arial',
+			color: _options.color || '#000',
+			background: _options.background || '#fff',
+			borderColor: _options.borderColor || '#888',
+		}))
+	}
+
+	return {
+		init: _init,
+		setImg: _setImg,
+	}
+}())
 
 /*************************************************************************************
 * コメント
